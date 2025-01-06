3841037c5a1676c93837f7d469e7aa199d2a5388
Marcos Allan <allanmenezes880@gmail.com>
2025-01-06 20:59:59 -0300

    Atualizei o tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,32 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {
+//       colors:{
+//         'my-primary': '#262521',
+//         'my-secondary': '#403F3C',
+//         'my-terciary': '#21CECC',
+//         'my-quartenary': '#F2F2F2',
+//         'my-quintenary': '#BFBFBF',
+//       },
+//       fontFamily:{
+//         montserrat: ['Montserrat', 'sans-serif']
+//       }
+//     },
+//   },
+//   plugins: [
+//     require('tailwind-scrollbar'),
+//   ],
+// }
+
+import { defineConfig } from 'tailwindcss';
+
+export default defineConfig({
+  content: ['index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors:{
@@ -21,5 +44,4 @@ export default {
   plugins: [
     require('tailwind-scrollbar'),
   ],
-}
-
+});
