d81568dc736cf9bfe48c0727ed0e1a2ffc401ca4
Marcos Allan <allanmenezes880@gmail.com>
2025-01-06 21:43:05 -0300

    adicionei arquivo de configuração vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,46 +1,24 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [
-//     "./index.html",
-//     "./src/**/*.{js,ts,jsx,tsx}",
-//   ],
-//   theme: {
-//     extend: {
-//       colors:{
-//         'my-primary': '#262521',
-//         'my-secondary': '#403F3C',
-//         'my-terciary': '#21CECC',
-//         'my-quartenary': '#F2F2F2',
-//         'my-quintenary': '#BFBFBF',
-//       },
-//       fontFamily:{
-//         montserrat: ['Montserrat', 'sans-serif']
-//       }
-//     },
-//   },
-//   plugins: [
-//     require('tailwind-scrollbar'),
-//   ],
-// }
-
-import scrollbar from 'tailwind-scrollbar';
-
-export default defineConfig({
-  content: ['index.html', './src/**/*.{js,jsx,ts,tsx}'],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
-      colors: {
+      colors:{
         'my-primary': '#262521',
         'my-secondary': '#403F3C',
         'my-terciary': '#21CECC',
         'my-quartenary': '#F2F2F2',
         'my-quintenary': '#BFBFBF',
       },
-      fontFamily: {
-        montserrat: ['Montserrat', 'sans-serif'],
-      },
+      fontFamily:{
+        montserrat: ['Montserrat', 'sans-serif']
+      }
     },
   },
-  plugins: [scrollbar],
-});
-
+  plugins: [
+    require('tailwind-scrollbar'),
+  ],
+}
